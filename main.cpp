59eeb33d8e6c69387818a25df9f2c05c8ec4e48e
ONETMSI\ONAT <onat.korkmaz@ug.bilkent.edu.tr>
2021-02-23 14:27:40 +0300

    onat-dev: added nilsu

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Hello, Onat Korkmaz 21704028!" << std::endl;
     std::cout << "Hello, Onat Postaci!" << std::endl;
+    std::cout << "Hello, Nilsu Urkmez!" << std::endl;
     return 0;
 }
