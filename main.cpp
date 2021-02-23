dcd83c29a561723a945aef36f1a4eecda75e341b
ONETMSI\ONAT <onat.korkmaz@ug.bilkent.edu.tr>
2021-02-23 14:26:58 +0300

    master: added numbers and sum

--- main.cpp
+++ main.cpp
@@ -3,5 +3,8 @@
 int main() {
     std::cout << "Hello, Onat Korkmaz 21704028!" << std::endl;
     std::cout << "Hello, Onat Postaci!" << std::endl;
+    int a = 3;
+    int b = 5;
+    int c = a + b;
     return 0;
 }
